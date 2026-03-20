5546be8083978c760c33133e8d71d559cea31931
Synneither-arch <251495054@qq.com>
2026-03-20 19:46:36 +0800

    添加 HashMap 示例，插入并打印分数

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,12 @@ fn main() {
     for i in &mut v {
         println!("{i}");
     }
-    use std::collections::hash_map;
+    use std::collections::HashMap;
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+    for (key,value) in &scores {
+        println!("{key}: {value}");
+        
+    }
 }
