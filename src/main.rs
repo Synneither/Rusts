c5a34bd8d8d5dbf46defe44c98af98ebd630a597
Synneither-arch <251495054@qq.com>
2026-04-15 09:40:45 +0800

    添加文件读取功能，处理文件打开错误，并新增 hello.txt 文件

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::fs::File;
+use std::io::{self,Read};
 fn main() {
     let mut v = vec![100, 37, 52];
     for i in &mut v {
@@ -7,8 +9,24 @@ fn main() {
     let mut scores = HashMap::new();
     scores.insert(String::from("Blue"), 10);
     scores.insert(String::from("Yellow"), 50);
-    for (key,value) in &scores {
+    for (key, value) in &scores {
         println!("{key}: {value}");
-        
+    }
+    let file_result = File::open("hello.txt");
+    let file = match file_result {
+        Ok(file) => panic!("File opened successfully: {file:?}"),
+        Err(error) => panic!("Problem opening the file: {error:?}"),
+    };
+}
+fn read_username_from_file() -> Result<String, std::io::Error> {
+    let file_result = File::open("hello.txt");
+    let mut file = match file_result {
+        Ok(file) => file,
+        Err(error) => return Err(error),
+    };
+    let mut username = String::new();
+    match file.read_to_string(&mut username) {
+        Ok(_) => Ok(username),
+        Err(error) => Err(error),
     }
 }
